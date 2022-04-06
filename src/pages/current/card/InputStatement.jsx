a995f1c5cd9c61e63c568f8cd6cc16b3155ae333
Leonid2104 <l.brdsh-41@yandex.ru>
2022-04-06 10:48:39 +0300

    status_upload_file

--- src/pages/current/card/InputStatement.jsx
+++ src/pages/current/card/InputStatement.jsx
@@ -10,6 +10,8 @@ import ip from 'ui/scssModule/input.module.scss'
 function InputStatement ({ innerLogScroll }) {
   const [comment, setComment] = useState('')
   const dispatch = useDispatch()
+  
+  const [statusUp, setStatus] = useState('non_active_status')
 
   const { currentCart } = useSelector(state => state.newCart)
   const { user } = useSelector(state => state.user)
@@ -26,39 +28,47 @@ function InputStatement ({ innerLogScroll }) {
   }
 
   async function uploadFile (e) {
+    setStatus('orange_status')
     const file = e.target.files[0]
     const formdata = new FormData()
     formdata.append('file', file)
     const result = await CartService.uploadFile(formdata, currentCart._id, user.id).then(data => data.data)
     dispatch(_getCurrentCart(result))
+    setStatus('green_status')
+    setTimeout(() => {
+      setStatus('non_active_status')
+    },2500)
   }
 
   return (
-    <div className='currentcart__content-message'>
-      <input
-        className={ip.inp}
-        type='text'
-        value={comment}
-        onChange={e => setComment(e.target.value)}
-        placeholder='Введите комментарий'
-        autoComplete='off'
-        onKeyUp={e => sendComment(e, 1)}
-        maxLength={500}
-      />
-      <div className='currentcart__content-btns'>
-        <label>
-          <img alt='' />
-          <input
-            type='file'
-            onChange={uploadFile}
-            accept='.doc,.docx,.jpg,.png,.txt,.xlsx,.pdf,.gif,.tar,.zip'
-          />
-        </label>
-
-        <button
-          onClick={e => sendComment(e, 2)}
-          className='currentcart__content-send'
+    <div className="">
+      <div className={statusUp}>{statusUp === 'orange_status' ? 'Идёт загрузка файла ...' : 'Файл успешно загружен'}</div>
+      <div className='currentcart__content-message'>
+        <input
+          className={ip.inp}
+          type='text'
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+          placeholder='Введите комментарий'
+          autoComplete='off'
+          onKeyUp={e => sendComment(e, 1)}
+          maxLength={500}
         />
+        <div className='currentcart__content-btns'>
+          <label>
+            <img alt='' />
+            <input
+              type='file'
+              onChange={uploadFile}
+              accept='.doc,.docx,.jpg,.png,.txt,.xlsx,.pdf,.gif,.tar,.zip'
+            />
+          </label>
+
+          <button
+            onClick={e => sendComment(e, 2)}
+            className='currentcart__content-send'
+          />
+        </div>
       </div>
     </div>
   )
